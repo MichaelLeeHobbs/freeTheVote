de9cf1e8e318a2db72b0bb85da47226fa3dbbf94
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-10-18 10:27:09 -0400

    vote view can now vote

--- client/app/vote/vote.controller.js
+++ client/app/vote/vote.controller.js
@@ -10,23 +10,43 @@ angular.module('freeTheVoteApp')
     self.newOption = '';
 
     $http.get('/api/polls').then(function (response) {
-      response.data.forEach(function(ele){
+      response.data.forEach(function (ele) {
         if (ele._id === $routeParams.pollId) {
           self.poll = ele;
         }
       });
       if (self.poll === undefined) {
-        $location.path( '/' );
+        $location.path('/');
       }
     });
 
     $scope.onClick = function () {
-      $location.path( '/poll' );
+      $location.path('/poll');
     };
 
-    $scope.onSubmit = function (form) {
+    $scope.onSubmit = function () {
       console.log(self.selection);
       console.log(self.newOption);
+      if (self.selection === -1) {
+        return;
+      }
+
+      if (self.selection !== -2) {
+        self.poll.votes[self.selection]++;
+      }
+
+      if (self.selection === -2) {
+        self.poll.votes.push(1);
+        self.poll.options.push(self.newOption);
+      }
+
+      $http.put('/api/polls/' + $routeParams.pollId + '/vote', self.poll)
+        .then(function (res) {
+          self.poll = res.data;
+        }).catch(function(err){
+          console.log('error: ' + err);
+        });
+
     };
 
     $scope.message = $routeParams.pollId;
