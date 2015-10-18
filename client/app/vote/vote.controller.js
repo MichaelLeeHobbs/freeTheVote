2d51c13857e28e360882214b65f21f7591a57256
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-10-18 11:07:26 -0400

    can now change vote

--- client/app/vote/vote.controller.js
+++ client/app/vote/vote.controller.js
@@ -11,6 +11,10 @@ angular.module('freeTheVoteApp')
     self.cookie = $cookies.get($routeParams.pollId);
     self.hasVoted = (self.cookie !== undefined);
 
+    if (self.hasVoted) {
+      self.selection = self.cookie;
+    }
+
     console.log(self.cookie);
 
     $http.get('/api/polls').then(function (response) {
@@ -31,8 +35,15 @@ angular.module('freeTheVoteApp')
     $scope.onSubmit = function () {
       console.log(self.selection);
       console.log(self.newOption);
-      if (self.selection === -1 || self.hasVoted) {
+      if (self.selection === -1) {
         return;
+      }
+
+      if (self.hasVoted) {
+        if (self.poll.votes[self.cookie] > 0) {
+          self.poll.votes[self.cookie]--;
+        }
+        self.cookie = -1;
       }
 
       if (self.selection !== -2) {
@@ -40,15 +51,21 @@ angular.module('freeTheVoteApp')
       }
 
       if (self.selection === -2) {
+        // see if the option already exist
+
+
+
         self.poll.votes.push(1);
         self.poll.options.push(self.newOption);
-        self.selection = self.poll.votes.size() - 1;
+        self.selection = self.poll.votes.length - 1;
       }
 
       $http.put('/api/polls/' + self.poll._id + '/vote', self.poll)
         .then(function (res) {
           self.poll = res.data;
+          self.hasVoted = true;
           $cookies.put(self.poll._id, self.selection);
+          self.cookie = $cookies.get($routeParams.pollId);
         }).catch(function(err){
           console.log('error: ' + err);
         });
