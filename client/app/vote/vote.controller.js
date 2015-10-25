b4d0bf560219e5dd88b9a5737de2b2fcb62e002b
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-10-25 09:36:06 -0400

    added delete functionality for owner

--- client/app/vote/vote.controller.js
+++ client/app/vote/vote.controller.js
@@ -1,29 +1,31 @@
 'use strict';
 
 angular.module('freeTheVoteApp')
-  .controller('VoteCtrl', function ($scope, $http, $routeParams, $location, $cookies) {
+  .controller('VoteCtrl', function ($scope, $http, $routeParams, $location, $cookies, Auth) {
     var self = this;
 
     // undefined = nothing selected
     // new = new option selected
     self.selection = undefined;
     self.newOption = '';
-    self.oldVote = $cookies.get($routeParams.pollId);
-    self.hasVoted = (self.oldVote !== undefined);
+    self.oldVote   = $cookies.get($routeParams.pollId);
+    self.hasVoted  = (self.oldVote !== undefined);
+    Auth.getCurrentUser(function (user) {
+      self.currentUserId = user._id;
+      console.log(user);
+    });
+    self.owner     = false;
 
     if (self.hasVoted) {
       self.selection = self.oldVote;
     }
 
-    $http.get('/api/polls').then(function (response) {
-      response.data.forEach(function (ele) {
-        if (ele._id === $routeParams.pollId) {
-          self.poll = ele;
-        }
-      });
-      if (self.poll === undefined) {
-        $location.path('/');
-      }
+    $http.get('/api/polls/' + $routeParams.pollId).then(function (response) {
+      console.log(response);
+      self.poll    = response.data;
+      self.isOwner = (self.poll.ownerId === self.currentUserId);
+    }).catch(function () {
+      $location.path('/');
     });
 
     $scope.onClick = function () {
@@ -47,8 +49,8 @@ angular.module('freeTheVoteApp')
       if (self.selection === 'new') {
 
         // see if the option already exist
-        var result = self.poll.options.some(function(ele, i){
-          if (ele.toString().toLowerCase() === self.newOption.toLowerCase()){
+        var result = self.poll.options.some(function (ele, i) {
+          if (ele.toString().toLowerCase() === self.newOption.toLowerCase()) {
             // new option already exist so set selection to index
             self.selection = i;
             return true;
@@ -60,7 +62,7 @@ angular.module('freeTheVoteApp')
           self.poll.votes.push(1);
           self.poll.options.push(self.newOption);
           self.selection = self.poll.votes.length - 1;
-          self.hasVoted = true;
+          self.hasVoted  = true;
         }
       }
 
@@ -71,15 +73,24 @@ angular.module('freeTheVoteApp')
 
       $http.put('/api/polls/' + self.poll._id + '/vote', self.poll)
         .then(function (res) {
-          self.poll = res.data;
+          self.poll     = res.data;
           self.hasVoted = true;
           $cookies.put(self.poll._id, self.selection);
-          self.oldVote = $cookies.get($routeParams.pollId);
-        }).catch(function(err){
+          self.oldVote  = $cookies.get($routeParams.pollId);
+        }).catch(function (err) {
           console.log('error: ' + err);
         });
 
     };
 
     $scope.message = $routeParams.pollId;
-  });
+    $scope.delete  = function () {
+      console.log('deleted');
+
+      $http.delete('/api/polls/' + $routeParams.pollId).then(function (response) {
+        console.log(response);
+      });
+
+    };
+  })
+;
