98b60f9bfd03be03218524da5ce92d37f365b7ab
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-10-25 12:40:03 -0400

    formatting

--- client/app/poll/poll.controller.js
+++ client/app/poll/poll.controller.js
@@ -2,15 +2,15 @@
 
 angular.module('freeTheVoteApp')
   .controller('PollCtrl', function ($scope, $http, $location) {
-    var self   = this;
-    this.polls = [];
+    var self = this;
 
-    $http.get('/api/polls').then(function (response) {
-      self.polls = response.data;
-      self.polls.forEach(function (ele){
-        ele.onClick = function (){
-          $location.path( '/vote/' + ele._id );
-        };
+    $http.get('/api/polls')
+      .then(function (response) {
+        self.polls = response.data;
+        self.polls.forEach(function (ele) {
+          ele.onClick = function () {
+            $location.path('/vote/' + ele._id);
+          };
+        });
       });
-    });
   });
