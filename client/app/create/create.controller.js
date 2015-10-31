c6e307652b48a76bb542f0f52640858f0c4614c3
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-10-31 10:33:32 -0400

    fixed post poll creation redirect... again

--- client/app/create/create.controller.js
+++ client/app/create/create.controller.js
@@ -43,7 +43,7 @@ angular.module('freeTheVoteApp')
       if (form.$valid) {
         $http.post('/api/polls/', self.poll)
           .then(function (res) {
-            $location.path( '/poll/' + res.data._id );
+            $location.path( '/poll/' + res.data._id + '/polls' );
             console.log(res);
           }).catch(function (err) {
             console.log('error: ' + err);
